66b75fd16a7189d1bea8c378e703cb3f42f3ba9f
abdallahosama9 <153902285+abdallahosama9@users.noreply.github.com>
2024-06-24 08:49:33 -0700

    create project

---
+++ script.js
@@ -0,0 +1,10 @@
+let img=document.querySelector(".img")
+let container=document.querySelector(".container")
+
+function move(par){
+img.src=par;
+
+  
+}function colors(vvv){
+    container.style.background=vvv
+}
